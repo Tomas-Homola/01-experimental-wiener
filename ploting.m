051d601b458ed95f5244690b46313fa6fad7c573
Tomas-Homola <homola.tom6@gmail.com>
2022-02-02 22:54:08 +0100

    Priebezny commit, asi nic nove

--- ploting.m
+++ ploting.m
@@ -3,6 +3,7 @@ data100 = readmatrix('data100.csv');
 data1000 = readmatrix('data1000.csv');
 data10000 = readmatrix('data10000.csv');
 
+% kontrolovat hodnoty pri poziciach 101, 201 a 301
 %% 100 trajektorii
 [count, ~] = size(data100);
 count = count - 1;
@@ -13,9 +14,9 @@ for n = 2:count
 end
 hold off
 
-mean_ = mean(data10000(2:end,2));
-std_ = std(data10000(2:end,2));
-dispersion_ = var(data10000(2:end,2));
+%mean_ = mean(data10000(2:end,2));
+%std_ = std(data10000(2:end,2));
+%dispersion_ = var(data10000(2:end,2));
 %% 1000 trajektorii
 [count, ~] = size(data1000);
 count = count - 1;
